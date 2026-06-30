561ca5ea4ea5a1ea9a148e7b369109a84b8a6c75
Kherilirli <januarkherilirli@gmail.com>
2026-07-01 00:48:30 +0700

    update lagi6

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,27 @@
 require("dotenv").config();
 
-process.on("uncaughtException", (err) => {
-    console.error("UNCAUGHT EXCEPTION");
-    console.error(err);
-});
+const mysql = require("mysql2/promise");
 
-process.on("unhandledRejection", (err) => {
-    console.error("UNHANDLED REJECTION");
-    console.error(err);
-});
+(async () => {
+    try {
+        console.log("START TEST");
 
-console.log("1. SERVER START");
-
-try {
-
-    const app = require("./app");
-    console.log("2. APP LOADED");
-
-    const db = require("./database/config");
-    console.log("3. DB LOADED");
-
-    const PORT = process.env.PORT || 3000;
-    console.log({
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    DB_USER: process.env.DB_USER,
-    DB_NAME: process.env.DB_NAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    });
-
-    db.getConnection()
-        .then(() => {
-            console.log("4. DATABASE CONNECTED");
-
-            app.listen(PORT, () => {
-                console.log(`5. SERVER RUNNING ${PORT}`);
-            });
-        })
-        .catch(err => {
-            console.error("DATABASE ERROR");
-            console.error(err);
+        const conn = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            connectTimeout: 10000
         });
 
-} catch (err) {
+        console.log("CONNECTED!");
 
-    console.error("IMPORT ERROR");
-    console.error(err);
+        await conn.end();
 
-}
+    } catch (err) {
+
+        console.error(err);
+
+    }
+})();
