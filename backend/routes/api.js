21826cc834bdcf1e55679484642e0ee132773c24
Kherilirli <januarkherilirli@gmail.com>
2026-07-01 00:21:59 +0700

    update lagi3

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -7,36 +7,47 @@ const {
     authenticate,
     authorize
 } = require('../middleware/auth');
-
+console.log("2. middleware loaded");
 
 const WhoController =
     require("../controllers/WhoController");
+console.log("3. WhoController");
 
 const AuthController =
     require('../controllers/authController');
+console.log("4. AuthController");
 
 const ReportController =
     require('../controllers/reportController');
+console.log("5. ReportController");
 
 const ChildController =
     require('../controllers/ChildController');
+console.log("6. ChildController");
 
-const NutritionController =
-    require('../controllers/NutritionController');
+const NutritionController = require("../controllers/NutritionController");
+console.log("7. NutritionController");
 
-const ActivityLogController =
-    require('../controllers/ActivityLogController');
+const ActivityLogController = require("../controllers/ActivityLogController");
+console.log("8. ActivityLogController");
 
-const NotificationController =
-    require('../controllers/NotificationController');
+const NotificationController = require("../controllers/NotificationController");
+console.log("9. NotificationController");
 
-const GrowthController =
-    require('../controllers/GrowthController');
-const RecommendationController = require('../controllers/RecommendationController');
-const ScheduleController = require('../controllers/ScheduleController');
-const DashboardController = require('../controllers/DashboardController');
-const UserController = require('../controllers/UserController');
+const GrowthController = require("../controllers/GrowthController");
+console.log("10. GrowthController");
 
+const RecommendationController = require("../controllers/RecommendationController");
+console.log("11. RecommendationController");
+
+const ScheduleController = require("../controllers/ScheduleController");
+console.log("12. ScheduleController");
+
+const DashboardController = require("../controllers/DashboardController");
+console.log("13. DashboardController");
+
+const UserController = require("../controllers/UserController");
+console.log("14. UserController");
 
 
 router.post(
